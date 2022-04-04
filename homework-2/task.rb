a859d491e1056ec32d746240633f3b8a95c5579a
ink80 <igorkozachuk@gmail.com>
2022-04-04 15:52:14 +0300

    code refactoring

--- homework-2/task.rb
+++ homework-2/task.rb
@@ -1,7 +1,5 @@
 
 def greeting(name, surname, age)
-
-
   if age < 18
     "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать программировать никогда не рано"
   else
@@ -20,21 +18,3 @@ print("Введите ваш возраст: ")
 age = gets.chomp.to_i
 
 puts greeting(name, surname, age)
-
-
-def foobar(a, b)
-  if a == 20 or b == 20
-    b
-  else
-    a+b
-  end
-end
-
-
-print("Введите первое число: ")
-a = gets.chomp.to_i
-
-print("Введите второе число: ")
-b = gets.chomp.to_i
-
-puts foobar(a, b)
